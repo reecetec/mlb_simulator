9ecfa40c270662a629a6300687349b7b321d3753
reecetec <colclour@mcmaster.ca>
2024-04-30 12:09:18 -0400

    completed statcast data upload

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -1,1 +1,30 @@
 # connects to SQL database from data/raw and provides functions to query data
+from pathlib import Path
+
+def get_db_locations(max_depth=10):
+    """Gets path for db file
+
+    Args:
+        max_depth (int, optional): max times to move back a directory. Defaults to 10.
+
+    Raises:
+        FileNotFoundError: uses README.md to locate root of project directory, so if not found an error is thrown
+
+    Returns:
+        strings: paths to database and table_schema json
+    """
+    
+    current_dir = Path(__file__).resolve().parent
+
+    for _ in range(max_depth):
+        if (current_dir / 'README.md').exists():
+            break
+        current_dir = current_dir.parent
+    else:
+        raise FileNotFoundError("README.md not found within the specified depth.")
+
+    # Append path to db to parent dir
+    db_path = current_dir / 'data' / 'databases' / 'mlb.db'
+    table_schema_path = current_dir / 'data' / 'databases' / 'table_schema.json'
+
+    return db_path, table_schema_path
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,9 +3,45 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 import sqlite3
 import json
+from sqlalchemy import create_engine
+import pandas as pd
+from datetime import datetime, timedelta
+from pybaseball import statcast, cache
+from data_utils import get_db_locations
+
+#DB_LOCATION = '../../data/databases/mlb.db'
+#TABLE_SCHEMAS_PATH = '../../data/databases/table_schema.json'
+DB_LOCATION, TABLE_SCHEMAS_PATH = get_db_locations()
 
 logger = logging.getLogger(__name__)
-DB_LOCATION = '../../data/databases/mlb.db'
+
+def validate_schema_path():
+    """Validates table schema file exists, otherwise script will end
+    """
+    if not Path(TABLE_SCHEMAS_PATH).exists():
+        logger.critical(f'table_schema.json not found at {TABLE_SCHEMAS_PATH}')
+        exit()
+
+def get_table_schema(table_name):
+    """Check if table is defined in table_schema json, and if so, return it, otherwise, throw error
+
+    Args:
+        table_name (Str): table name of schema to look up
+
+    Returns:
+        Dict: table schema 
+    """
+    with open(TABLE_SCHEMAS_PATH, 'r') as file:
+        schema = json.load(file)
+    
+    # validate table schema exists
+    if table_name in schema:
+        table_schema = schema[table_name]
+        return table_schema
+    else:
+        logger.critical(f'no {table_name} schema found in table_schema.json')
+        exit()
+
 
 def create_table(table_name = 'Statcast'):
     """Creates tables in data/databases/mlb.db for table schemas found in the table_schema.json file
@@ -13,61 +49,88 @@ def create_table(table_name = 'Statcast'):
     Args:
         table_name (str, optional): name of table to initialize from table_schema.json file. Defaults to 'Statcast'.
     """
-    
-    table_schemas_path = '../../data/databases/table_schema.json'
-    DB_LOCATION = '../../data/databases/mlb.db'
+    validate_schema_path()
+    table_schema = get_table_schema(table_name)
 
-    if Path(table_schemas_path).exists():
-        
-        # read schema/create table script
-        with open(table_schemas_path, 'r') as file:
-            schema = json.load(file)
+    # initialize db
+    conn = sqlite3.connect(DB_LOCATION)
+    c = conn.cursor()
+    # generate create table string from schema
+    create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ("
+    for column in table_schema['columns']:
+        create_table_sql += f"{column['name']} {column['type']}, "
+    # add the primary key constraint
+    if 'primaryKey' in table_schema:
+        create_table_sql += f"PRIMARY KEY ({', '.join(table_schema['primaryKey'])})"
+    create_table_sql += ")"
 
-        # validate table schema exists
-        if table_name in schema:
-            table_schema = schema[table_name]
-        else:
-            logger.critical(f'no {table_name} schema found in table_schema.json')
-            exit()
+    # execute the SQL statement
+    c.execute(create_table_sql)
+    conn.commit()
+    conn.close()
+    logger.info(f'Creation successful for {table_name}')
 
+def update_table(table_name):
 
-        # initialize db
-        conn = sqlite3.connect(DB_LOCATION)
-        c = conn.cursor()
-        
-        # generate create table string from schema
-        create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ("
-        for column in table_schema['columns']:
-            create_table_sql += f"{column['name']} {column['type']}, "
-        # add the primary key constraint
-        if 'primaryKey' in table_schema:
-            create_table_sql += f"PRIMARY KEY ({', '.join(table_schema['primaryKey'])})"
-        create_table_sql += ")"
-
-        # execute the SQL statement
-        c.execute(create_table_sql)
-
-        conn.commit()
-        conn.close()
-
-        logger.info(f'Success: "{table_name}"')
-    
-    else:
-        logger.critical(f'table_schema.json not found at {table_schemas_path}')
+    if not Path(DB_LOCATION).exists():
+        logger.critical('mlb.db doesnt exist, something went wrong in the creation')
         exit()
+
+    validate_schema_path()
+    table_schema = get_table_schema(table_name)
+
+    engine = create_engine(f'sqlite:///{DB_LOCATION}', echo=False)
+    max_date = pd.read_sql('select max(game_date) from Statcast',engine)['max(game_date)'][0]
     
+    cache.enable()
+    today = datetime.today().strftime('%Y-%m-%d')
+    # if no data uploaded, get all data from 2017 to today.
+    if max_date==None:
+        df = statcast('2017-01-01', '2020-01-01')#today)
+        if not df.empty:
+
+            # drop garbage
+            df = df.drop(columns=['pitcher.1','fielder_2.1'], errors='ignore')
+
+            # ensure no duplicates
+            cur_keys = pd.read_sql('select game_date, game_pk, at_bat_number, pitch_number from statcast', engine)
+            key_match_df = pd.merge(df, cur_keys, how='inner', on=['game_date', 'game_pk', 'at_bat_number', 'pitch_number'])
+            df.drop(key_match_df.index, inplace=True)
+
+            df.to_sql('Statcast', engine, if_exists='append', index=False,
+                      chunksize=1000)
+        logger.info('Successfully uploaded statcast data from 2017 to 2019')
+
+    # otherwise, get all data from 
+    else:
+        max_date = datetime.strptime(max_date, '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d')
+        key_date = (datetime.strptime(max_date, '%Y-%m-%d') - timedelta(days=5)).strftime('%Y-%m-%d')
+        df = statcast(max_date, today)
+        if not df.empty:
+            # drop garbage
+            df.drop(columns=['pitcher.1','fielder_2.1'], errors='ignore', inplace=True)
+
+            # ensure no duplicates
+            cur_keys = pd.read_sql(f'select game_date, game_pk, at_bat_number, pitch_number from statcast where game_date > {key_date}', engine)
+            cur_keys['game_date'] = pd.to_datetime(cur_keys['game_date'])
+            key_match_df = pd.merge(df, cur_keys, how='inner', on=['game_date', 'game_pk', 'at_bat_number', 'pitch_number'])
+            df.drop(key_match_df.index, inplace=True)
+
+            df.to_sql('Statcast', engine, if_exists='append', index=False,
+                      chunksize=1000)
+        logger.info('Successfully updated statcast data')
+
 
 def main():
     logger.info('Starting SQLite db creation/updates')
 
-    #make sure all desired tables are created
+    #For each table in the list, create the table and/or update it
     required_tables = ['Statcast']
-    for table in required_tables:
-        logger.info(f'Validating "{table}" exists')
-        create_table(table)
-        logger.info(f'Updating "{table}"')
 
-    #run updates on tables
+    for table in required_tables:
+        create_table(table)
+        logger.info(f'Updating {table}')
+        update_table(table)
 
     logger.info('DB creation and updates complete')
 
