123ff26dddf45cbf809463102135a1c25e5094e4
reecetec <colclour@mcmaster.ca>
2024-05-02 11:47:55 -0400

    init functions for feature creation

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,4 +1,18 @@
 #using functions from data utils, constructs a featureset for a model
 
-from data import data_utils as du
+from ..data.data_utils import query_mlb_db
+
+
+def get_pitch_generation_features(pitcher_id, batter_id, game_state):
+    pass
+
+
+def get_pitch_outcome_features(batter_id, pitch_characteristics):
+    pass
+
+def get_bat_outcome_features(batter_id):
+    pass
+
+def get_field_outcome_features():
+    pass
 
